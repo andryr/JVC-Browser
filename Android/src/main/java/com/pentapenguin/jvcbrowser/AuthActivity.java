81bd46b04edc4aa330baacbfbbe8883f04fe43df
Andry Rafaralahy <arafaralahy@gmail.com>
2016-01-02 20:16:03 +0100

    remplacement du formulaire de login par une WebView

--- Android/src/main/java/com/pentapenguin/jvcbrowser/AuthActivity.java
+++ Android/src/main/java/com/pentapenguin/jvcbrowser/AuthActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.*;
 import com.pentapenguin.jvcbrowser.app.App;
 import com.pentapenguin.jvcbrowser.app.Auth;
@@ -21,133 +24,56 @@ import java.util.HashMap;
 public class AuthActivity extends Activity {
 
     private static final String URL_LOGIN = App.HOST_MOBILE + "/sso/login.php";
-    private static final String CLASS_FORM = "form-connect";
-    private static final String CLASS_ERROR = "alert-danger";
-    private static final String CLASS_CAPTCHA = "bloc-captcha";
+
     public static final int RESULT_CODE = 42;
 
-    private HashMap<String, String> mData;
-    private HashMap<String, String> mCookies;
-    private Button mConnect;
-    private EditText mPseudo;
-    private EditText mPasswd;
-    private ImageView mCaptcha;
-    private EditText mCode;
-    private TextView mError;
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(Theme.authActivity);
 
-        mData = new HashMap<String, String>();
-        mCookies = new HashMap<String, String>();
-        mConnect = (Button) findViewById(R.id.button_connect);
-        mPseudo = (EditText) findViewById(R.id.text_pseudo);
-        mPasswd = (EditText) findViewById(R.id.text_passwd);
-        mCaptcha = (ImageView) findViewById(R.id.image_connect_captcha);
-        mCode = (EditText) findViewById(R.id.edit_connect_captcha);
-        mError = (TextView) findViewById(R.id.text_connect_error);
 
-        init();
+        mWebView = (WebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(URL_LOGIN);
 
-    }
-
-    private void init() {
-        final ProgressDialog dialog = App.progress(this, R.string.in_progress, true);
-
-        dialog.show();
-        Ajax.url(URL_LOGIN).callback(new AjaxCallback() {
+        mWebView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onComplete(Connection.Response response) {
-                dialog.dismiss();
-                if (response != null) {
-                    try {
-                        mData.putAll(Parser.hidden(response.parse(), CLASS_FORM));
-                        mCookies.putAll(response.cookies());
-
-                        return;
-                    } catch (IOException e) {
-                        App.alert(AuthActivity.this, e.getMessage());
+            public void onPageFinished(WebView view, String url) {
+                String cookies = CookieManager.getInstance().getCookie(App.HOST_MOBILE);
+                String[] temp = cookies.split(";");
+                for (String ar1 : temp ){
+                    if(ar1.contains(Auth.COOKIE_NAME)){
+                        int i = ar1.indexOf("=")+1;
+                        if(i<ar1.length()) {
+                            String cookie = ar1.substring(i);
+                            onConnected("", cookie);
+                        }
+                        break;
                     }
                 }
-//                App.alert(AuthActivity.this, R.string.no_response);
-                App.snack(getWindow().getDecorView().getRootView(), R.string.no_response);
-            }
-        }).execute();
-
-        mConnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.setMessage("Connexion en cours...");
-                dialog.show();
-                String psd = checkField(mPseudo);
-                String pass = checkField(mPasswd);
-                String code = checkField(mCode);
-
-                mData.put("login_pseudo", psd);
-                mData.put("login_password", pass);
-                if (!code.equals("")) mData.put("fs_ccode", code);
-                initWidgets();
-                Ajax.url(URL_LOGIN).post().data(mData).cookies(mCookies).callback(new AjaxCallback() {
-                    @Override
-                    public void onComplete(Connection.Response response) {
-                        dialog.dismiss();
-                        if (response != null) {
-                            try {
-                                Document doc = response.parse();
-                                String captcha = Parser.captcha(doc, CLASS_CAPTCHA);
-                                String error = Parser.error(doc, CLASS_ERROR);
-
-                                mData.clear();
-                                mData.putAll(Parser.hidden(doc, CLASS_FORM));
-                                if (captcha != null) onCaptcha(captcha);
-                                if (error != null) onError(error);
-                                for (String name : response.cookies().keySet()) {
-                                    if (name.equals(Auth.COOKIE_NAME)) onConnected(response.cookies().get(name));
-                                }
-
-                                return;
-                            } catch (IOException e) {
-                                App.alert(AuthActivity.this, e.getMessage());
-                            }
-                        }
-//                        App.alert(AuthActivity.this, R.string.no_response);
-                        App.snack(getWindow().getDecorView().getRootView(), R.string.no_response);
-                    }
-                }).execute();
             }
         });
+
+
     }
 
-    private String checkField(EditText edit) {
-        if (edit.getText() != null && !edit.getText().toString().equals(""))
-            return edit.getText().toString();
-        return "";
-    }
 
-    private void initWidgets() {
-        mCaptcha.setVisibility(View.GONE);
-        mCode.setVisibility(View.GONE);
-        mCode.setText("");
-        mError.setVisibility(View.GONE);
-    }
 
-    private void onCaptcha(String url) {
-        mCaptcha.setVisibility(View.VISIBLE);
-        mCode.setVisibility(View.VISIBLE);
-        Picasso.with(this).load(url).into(mCaptcha);
-    }
 
-    private void onConnected(String cookie) {
-        Auth.getInstance().connect(checkField(mPseudo), cookie);
+
+
+
+
+    private void onConnected(String pseudo, String cookie) {
+        Auth.getInstance().connect(pseudo, cookie);
         App.toast(R.string.connected);
         setResult(RESULT_CODE);
         finish();
     }
 
-    private void onError(String error) {
-        mError.setVisibility(View.VISIBLE);
-        mError.setText(error);
-    }
+
 }
